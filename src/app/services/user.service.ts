d4278d5897ffc12fde4e6e4cbb1e1e974f95140d
YunusEmreOZTURK <yunus@dekatechs.com>
2024-01-20 01:05:00 +0300

    Forgot Password Page Created

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -18,5 +18,13 @@ export class UserService {
       })
   }
 
+  
+  forgotPassword(data:any){
+    return this.httpClient.post(this.url+
+      "/user/forgotPassword",data,{
+        headers:new HttpHeaders().set('Content-Type','application/json')
+      })
+  }
+
 
 }
