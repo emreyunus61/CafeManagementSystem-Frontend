7d6d0bf63b5a1a85b24d63a714e9c541a22532c0
YunusEmreOZTURK <yunus@dekatechs.com>
2024-01-24 14:13:55 +0300

    Category Page Created

--- src/app/material-component/manage-category/manage-category.component.ts
+++ src/app/material-component/manage-category/manage-category.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { error } from 'console';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { CategoryService } from 'src/app/services/category.service';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+import { GlobalConstans } from 'src/app/shared/global-constants';
+import { CategoryComponent } from '../dialog/category/category.component';
 
 @Component({
   selector: 'app-manage-category',
@@ -7,9 +16,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageCategoryComponent implements OnInit {
 
-  constructor() { }
+ 
+  displayedColums: String[] = ['name','edit'];
+  dataSource:any;
+  responseMessage:any;
+
+  constructor(private categoryService:CategoryService,
+    private ngxService:NgxUiLoaderService,
+    private dialog:MatDialog,
+    private snackbarService:SnackbarService,
+    private router:Router) { }
 
   ngOnInit(): void {
+    this.ngxService.start();
+    this.tableData();
+  }
+
+  tableData(){
+    this.categoryService.getCategorys().subscribe((response:any)=>{
+      this.ngxService.stop();
+      this.dataSource = new MatTableDataSource(response);
+      console.log("Datasources",this.dataSource)
+    }, (error:any)=>{
+      this.ngxService.stop();
+      console.log(error.error?.message);
+      if(error.error?.message){
+        this.responseMessage=error.error?.message;
+      }else{
+        this.responseMessage= GlobalConstans.genericError;
+      }
+      this.snackbarService.openSnackBar(this.responseMessage,GlobalConstans.error);
+    })
+  }
+  //trim()=>(string) değerin başında veya  sonunda boşluk varsa boşlukları siler.
+  applyFilter(event:Event){
+    const filterValue=(event.target as HTMLInputElement).value;
+    this.dataSource.filter=filterValue.trim().toLocaleLowerCase();
+  }
+
+  handleAddAction(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = {
+      action: 'Add'
+    };
+    dialogConfig.width="850px";
+    const dialogRef = this.dialog.open(CategoryComponent,dialogConfig);
+    this.router.events.subscribe(()=>{
+      dialogRef.close();
+    });
+    //componentInstance` kullanılarak açılan bir dialog bileşenin içindeki olaylar (events) dinlenir 
+    //ve bu olaylar sayesinde alt bileşenin üst bileşeni etkilemesi sağlanır.
+    const sub = dialogRef.componentInstance.onAddCategory.subscribe((response)=>{
+      this.tableData();
+    })
+  }
+
+
+  handleEditAction(values:any){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = {
+      action: 'Edit',
+      data:values
+    };
+    dialogConfig.width="850px";
+    const dialogRef = this.dialog.open(CategoryComponent,dialogConfig);
+    this.router.events.subscribe(()=>{
+      dialogRef.close();
+    });
+    const sub = dialogRef.componentInstance.onAddCategory.subscribe((response)=>{
+      this.tableData();
+    })
+
   }
 
 }
